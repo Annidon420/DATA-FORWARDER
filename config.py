ed01b0719fe6df43fa2f44fe9af2b991ca5f8091
STARK 69 <166913769+Annidon420@users.noreply.github.com>
2026-02-20 09:34:55 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID"))
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
